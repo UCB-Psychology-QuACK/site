d0a3b9e5e4ce960de7930310f6915122bc4df38d
Elena Leib <eleib@berkeley.edu>
2021-09-20 13:54:37 -0700

    Create week4_starter_code.R

---
+++ QuACK2021/Week4/scripts/week4_starter_code.R
@@ -0,0 +1,19 @@
+# QuACK Week 4 - Tidyverse Part 2
+# September 21, 2021
+# Willa Voorhies & Elena Leib
+
+#### Load libraries and set options ####
+options(stringsAsFactors = FALSE)
+
+library(tidyverse)
+library(tidylog)
+
+
+#### Warm-up ####
+
+
+# FILL IN
+# Who is making the warmup?
+
+
+#### case_when() ####
