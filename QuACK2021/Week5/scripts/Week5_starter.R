155576396c45bb9bcbf13261ef96cfa2c9af0f69
wvoorhies <wvoorhies@berkeley.edu>
2021-09-28 15:15:42 -0700

    update

--- QuACK2021/Week5/scripts/Week5_starter.R
+++ QuACK2021/Week5/scripts/Week5_starter.R
@@ -18,35 +18,38 @@ penguins <- read.csv('../data/penguins_clean.csv')
 
 ## 1. Look at body mass by species
 
+ggplot(penguins, aes(species, body_mass_g)) + 
+  geom_col()
+
 # Set up our ggplot and define our variables
 
 
 # Map the data to a column object.
 
 
-# Notice that the species are ordered alphabetically. We could change this by creating an ordered factor. (eg. factor(species, levels = c("Chinstrap", "Adelie", "Gentoo"))
-
-
-## 2. Now lets see if there are differences between sexes 
-
-
-
 # Bar charts aren't the best way to look at data because it doesn't tell us much about individual data points or the distribution of data. 
 
-
-## 3.  Let's change the geometric representation of our data and use a boxplot. 
-
+## 2. Let's change the geometric representation of our data and use a boxplot. 
 
 
 # This gives us some more descriptive stats about the data but we still don't have a good feel for what the distribution of the data points look like.
 
-## 4. Lets use a violin plot to visualize the data. Violin plots are a great way to visualize the distribution of your data. 
 
+# 3. Lets use a violin plot to visualize the data. Violin plots are a great way to visualize the distribution of your data. 
 
 # stop the ends from being trimmed. 
 
 
 # Add a boxplot on top.
+
+
+# Notice that the species are ordered alphabetically. We could change this by creating an ordered factor. (eg. factor(species, levels = c("Chinstrap", "Adelie", "Gentoo"))
+
+
+## 4. Now lets see if there are differences between sexes 
+
+
+
 
 
 
