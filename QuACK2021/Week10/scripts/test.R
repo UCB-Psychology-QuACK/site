cc5f11d2f4852c98124ea298e7a0aa94a77e6075
wvoorhies <wvoorhies@berkeley.edu>
2021-11-08 17:41:06 -0800

    added condition data and steps 3-4 in practice to key

---
+++ QuACK2021/Week10/scripts/test.R
@@ -0,0 +1,218 @@
+# Week 10 - Functions
+# Elena & Willa
+# 11/2/21
+
+#### Load libraries and set options ####
+library(tidyverse)
+# options(stringsAsFactors = FALSE)
+
+
+
+#### Let's make some functions! ####
+
+# Let's use bootstrapping to estimate how much the mean body mass varies between
+# samples of penguins (standard error of the mean - SEM)
+
+# Load data
+penguins <- read.csv("../data/penguins_clean.csv") # (Note: only 2008 sample)
+
+
+# Here is what we did before:
+
+# Sample our sample size (101) 1000 times!
+
+# Vector to keep track of the sample means
+means <- c()
+for(i in 1:1000) {
+  # 1) Create a resampled data set, sample WITH REPLACEMENT
+  p.resampled <- penguins %>%
+    sample_n(nrow(penguins), replace = T)
+  
+  # 2) Calculate the mean of body mass and save it
+  means[i] <- mean(p.resampled$body_mass_g)
+  
+}
+
+# Plot our means
+hist(means)
+sd(means)
+
+
+
+# How could we make these steps more "flexible"?
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Let's make our sample size and num_samples more flexible!
+
+
+
+
+
+
+# Now, let's take this one step even further in making this flexible by making
+# it into a function called bootstrap_bodyMass
+
+
+
+
+
+# Let's add some additional functionality to our function.
+# Let's make it so we can set a seed that we want it to use so that we (and
+# others) can replicate our results.
+
+
+
+
+
+# But what if I don't always want to pass it a seed? Right now, if I don't pass
+# it a seed, the entire function doesn't work! We've added flexibility, but
+# we've also removed some at the same time...
+
+# To solve this problem, we can set a default value for an argument! This allows
+# us to make an argument optional, and only if we pass something as an argument
+# in the function will it override the default.
+
+# Let's make the default value NA because if we don't specifically set a seed,
+# we don't want there to be one, and we will just let R use the default seed. We
+# will also need to make some changes to the body of our function to fully
+# incorporate the change.
+
+
+
+
+
+
+
+
+
+# What other parts of this function would we want to make more flexible?
+
+
+
+
+
+
+
+
+
+
+# Maybe I don't only want to find the bootstrapped error of body_mass_g. Perhaps
+# I want the flexibility to find any of the variables I have!
+
+
+
+
+
+
+# ^^ should give us the same as what we got before with the _bodyMass function!
+means <- bootstrap_bodyMass(100, 1000, 1234)
+hist(means)
+
+# It does!
+
+
+# Let's try it with another variable, bill_length_mm
+
+
+
+# Does this make sense? Let's check the original data:
+mean(penguins$bill_length_mm)
+mean(means)
+
+sd(penguins$bill_length_mm) / sqrt(nrow(penguins))
+sd(means)
+# ^^ Looks good!
+
+
+
+# What if we wanted to do it with a different data set?
+# Load more data
+baby <- read.csv("../data/brainwavebabydata.csv")
+happiness <- read.csv("../data/world-happiness_2020.csv")
+
+
+
+
+
+
+
+
+
+# ^^ should give us the same as what we got before with the _penguins function!
+means <- bootstrap_penguins("body_mass_g", 100, 1000, 1234)
+hist(means, main = "body_mass_g")
+
+# It does!
+
+
+# Now, try it with another data set and variable. Then we will do some together.
+
+
+
+
+
+
+
+
+
+# If I were writing this function for my work, I would TEST TEST TEST! We would
+# need to make sure that everything is working properly!
+
+
+
+
+
+
+# Now that we have a working function, next thing is to DOCUMENT it so that we can easily share it with others!
+# The most important things to document about a function are 3-fold:
+# 1) What it does
+# 2) What its arguments are (both the type and what it is/what purpose it serves)
+# 3) What it returns
+
+# Let's do this documentation for our bootstrap_SEM function
+
+
+
+
+
+
+
+
+library(tidyverse)
+set.seed(1)
+sample_size <- 300
+dat <- tibble(
+  x1 = runif(sample_size, min = 0, max = 1),
+  e = rnorm(sample_size, sd = 0.1),
+  p = 1 / (1 + exp(-(-1 +  x1 + e))),
+  log_odds = log(p / (1 - p)),
+  y = rbinom(sample_size, 1, p)
+)
+
+
+fit <- glm(y ~ x1, data = dat, family = binomial)
+
+fit
+
+
+#### Good resources about functions ####
+# Basic intro to functions:
+# https://swcarpentry.github.io/r-novice-inflammation/02-func-R/
+
+# Intro to roxygen2 for more formally documenting functions that you make:
+# https://combine-australia.github.io/r-pkg-dev/documenting-functions.html
+
+
+
+read.csc
--- QuACK2021/Week11/week11_key.R
+++ QuACK2021/Week11/week11_key.R
@@ -24,7 +24,6 @@ str(wisc_raw)
 
 wisc_rowRM <- wisc_raw %>% 
   slice(-c(1, 2))
-
 # Could also use indexing!
 # wisc_rowRM <- wisc_raw[-c(1,2),]
 
@@ -89,8 +88,8 @@ wisc_processed <- wisc_raw %>%
          "item35" = Q68) %>% 
   
   # Make levels of condition meaningful! And fix the column types!!!!
-  mutate(condition = case_when(condition == "1" ~ "control",
-                               condition == "2" ~ "exp"),
+  mutate(condition = case_when(condition == "0" ~ "control",
+                               condition == "1" ~ "exp"),
     across(.cols = c(contains("item"), pid, totalDuration), as.numeric),
          across(.cols = c(grade, condition), as.factor))
 
@@ -147,3 +146,133 @@ write.csv(wisc_score, file = "wisc_part2-2.csv", row.names = FALSE)
 # It looks like the score that Qualtrics calculated for us (Q.score) is off by
 # one! Good thing that we recomputed it ourselves! We might want to look into
 # why it is doing that. But for now, we can move onto plotting.
+
+## First lets look at the general distributin of our measure of interest. 
+
+# We can check this by looking at the score first
+ggplot(wisc_score, aes(score, label = pid)) + 
+  geom_histogram(bins = 15)
+
+# in general our data has a good spread. But we see at least one participant has a score of zero. They may not be performing the task. 
+
+# Let's look at the duration of the task to see if anything happened during the administration of the task 
+ggplot(wisc_score, aes(totalDuration, label = pid)) + 
+  geom_histogram() 
+
+# Again we see that there is an notable outlier in task duration. This seems odd and we will probably want to remove this participant.  
+
+# Before I remove them I want to see who the outliers are and where they fall relative to their group. 
+ggplot(wisc_score, aes(grade, score , label = pid)) + 
+  # plot by group. Boxplot will show outliers who fall outside 1.5 times the IQR.
+  geom_boxplot() + 
+  # Include text to identify the subjects.
+  geom_text()
+# We see there are two subjects with a score of zero. We may want to remove these participants  they are not doing the task
+
+# Note: one of these participants is the one who has the very long duration so we will definitely remove them.  
+
+ggplot(wisc_score, aes(grade, totalDuration , label = pid)) + 
+  geom_boxplot() + 
+  geom_text()
+
+# Remove the two subjects with a score of 0. 
+wisc_score <- wisc_score %>% 
+  filter(pid != 61108  & 
+           pid !=72613 )
+
+# Check it worked. 
+ggplot(wisc_score, aes(score, label = pid)) + 
+  geom_histogram(bins = 15)
+  
+
+## Now we can move on to looking for group effects. We can make this a function so that we can use it again for other data. 
+
+violin_plot <- function(df, x, y, z) {
+  # A function to create violin plots. 
+  # INPUTS: 
+  # df <- a dataframe containing data for the plot.
+  # x <- a string corresponding to column name of a grouping variable for x-axis
+  # y <- a string corresponding to column name of a continuous variable for the y-axis. 
+  # z <- a string corresponding to the column name of a second grouping variable. 
+  # xaxis <- name of the xaxis as type string
+  # yaxis <- name of the yaxis as type string
+  #
+  # RETURNS:
+  # A vionlin plot. 
+  
+  p <- ggplot(df, aes(get(x), get(y), fill = get(z) )) + 
+    # set violin plot
+    geom_violin(trim = FALSE) + 
+    
+    # Add mean 
+    
+    stat_summary(fun = mean, geom = "point", size = 1, position = position_dodge(1)) +
+
+  
+    # set color scale
+    scale_fill_manual(name = z, values = c("#FD7F00", "#0141CF")) + 
+
+  
+    # set  axis labels
+    xlab(x) + 
+    ylab(y) + 
+    
+    # Set theme
+    theme_classic() 
+  
+      
+  
+  return (p)
+  
+}
+
+# Plot 
+violin_plot(wisc_score, 'grade', 'score', 'condition')
+
+
+## Now lets see if this difference between conditions is a true difference or random variation. To do this, we can use the idea behind permutation testing where we shuffle our condition labels so that they are randomly assigned. 
+
+# First, lets look at the mean difference in scores between control and experimental groups. We will collapse across grades here but feel free to try calculating this by group on your own! 
+
+# get the mean score for each group
+E.mean <- with(wisc_score, mean(wisc_score[condition == "exp",]$score))
+C.mean <- with(wisc_score, mean(wisc_score[condition == "control",]$score))
+
+# calculate the true mean difference. 
+true_mean_diff <- E.mean - C.mean
+  
+# Now lets determine whether this mean difference is a true group difference.
+# Let's create a new column randomly shuffled condition labels
+wisc_score$condition_shuffled <- sample(wisc_score$condition)
+
+# we can visualize the difference with our plot function. 
+violin_plot(wisc_score, 'condition_shuffled', 'score', 'grade')
+
+# Now this very different from our data,but lets shuffle the data many times to get a distribution of possible mean differences. 
+
+# create an empty vector to store mean differences
+reasoning.diffs <- c()
+
+# peform this 1000 times
+for(i in 1:1000) {
+
+  # Randomly shuffle the sex column WITHOUT REPLACEMENT!
+  wisc_score$condition_shuffled <- sample(wisc_score$condition)
+  
+  # Calculate the means for each group
+  C.mean_shuffled <- with(wisc_score, 
+                          mean(wisc_score[condition_shuffled=='exp', ]$score)) 
+  E.mean_shuffled <- with(wisc_score, 
+                          mean(wisc_score[condition_shuffled=='control', ]$score)) 
+  # Calcualte the difference in the means and save it
+  reasoning.diffs[i] <- (E.mean_shuffled - C.mean_shuffled)
+}
+
+# Lets plot our means and compare to the actual true mean difference. 
+hist(reasoning.diffs, 
+     xlim=c(-8, 8)) 
+# inclue a line showing our true group difference. 
+abline(v = true_mean_diff, col = "red", lwd = 3)
+
+# Even without running any statistics it is already pretty clear that our difference is significant! 
+
